02e834c9d06d7cef63388d9ac503fc8298fa0060
vatan-prakash <90102762+vatan-prakash@users.noreply.github.com>
2023-08-26 17:01:58 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Rotate Array/rotate-array.cpp
@@ -0,0 +1,59 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+     void rotateArr(int arr[], int d, int n){
+        // code here
+       
+        d=d%n;
+        reverse(arr,arr+d);
+        reverse(arr+d,arr+n);
+        reverse(arr,arr+n);
+    }
+    //Function to rotate an array by d elements in counter-clockwise direction. 
+    // void revrse(int* arr, int high, int low){
+    //     while(low<high){
+    //         swap(arr[low],arr[high]);
+    //         low++;
+    //         high--;
+    //     }
+    // }
+   
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	int t;
+	//taking testcases
+	cin >> t;
+	
+	while(t--){
+	    int n, d;
+	    
+	    //input n and d
+	    cin >> n >> d;
+	    
+	    int arr[n];
+	    
+	    //inserting elements in the array
+	    for(int i = 0; i < n; i++){
+	        cin >> arr[i];
+	    }
+	    Solution ob;
+	    //calling rotateArr() function
+	    ob.rotateArr(arr, d,n);
+	    
+	    //printing the elements of the array
+	    for(int i =0;i<n;i++){
+	        cout << arr[i] << " ";
+	    }
+	    cout << endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
