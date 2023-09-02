e788d14f5b47e8da86bca47ad4b5a47400a3c043
vatan-prakash <90102762+vatan-prakash@users.noreply.github.com>
2023-09-02 14:44:40 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Number of Provinces/number-of-provinces.cpp
@@ -0,0 +1,73 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+    private:
+    void dfs(int node, vector<int> adjlist[],int Vis[]){
+        Vis[node]=1;
+        for(auto it: adjlist[node]){
+            if(!Vis[it]){
+            dfs(it,adjlist,Vis);
+            }
+        }
+    }
+  public:
+    int numProvinces(vector<vector<int>> adj, int V) {
+        // code heretor
+        //changing adjency mtrix to adj list
+        vector<int> adjlist[V];
+        for(int i=0;i<V;i++){
+            for(int j=0;j<V;j++){
+                if(adj[i][j]==1 && i!=j){
+                    adjlist[i].push_back(j);
+                      adjlist[j].push_back(i);
+                }
+            }
+        }
+      
+        int Vis[V]={0};
+          int c =0;
+        for(int i=0;i<V;i++){
+          if(!Vis[i]){
+              c++;
+              dfs(i,adjlist,Vis);
+          }
+        }
+        return c;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int V,x;
+        cin>>V;
+        
+        vector<vector<int>> adj;
+        
+        for(int i=0; i<V; i++)
+        {
+            vector<int> temp;
+            for(int j=0; j<V; j++)
+            {
+                cin>>x;
+                temp.push_back(x);
+            }
+            adj.push_back(temp);
+        }
+        
+
+        Solution ob;
+        cout << ob.numProvinces(adj,V) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
