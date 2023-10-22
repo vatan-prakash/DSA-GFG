742d0749014e9934f536a5c202846cc9961f6955
vatan-prakash <90102762+vatan-prakash@users.noreply.github.com>
2023-10-22 07:13:31 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Jump Game/jump-game.cpp
@@ -0,0 +1,43 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    int canReach(int A[], int N) {
+        // code here
+    
+        int farthest=0;
+        for(int i=0;i<N;i++){
+            if(i>farthest) return 0;
+            farthest=max(farthest,i+A[i]);
+            if(farthest>=N-1) return true;
+        }
+        return 0;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin>>N;
+        
+        int A[N];
+        
+        for(int i=0; i<N; i++)
+            cin>>A[i];
+
+        Solution ob;
+        cout << ob.canReach(A,N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
