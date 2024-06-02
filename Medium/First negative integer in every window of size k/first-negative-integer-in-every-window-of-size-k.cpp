26268a1a7c1fff628e0ebbb8669fcbe9f08d29f9
vatan-prakash <90102762+vatan-prakash@users.noreply.github.com>
2024-06-02 11:08:06 +0530

    Updated Solution - GfG to GitHub

--- Medium/First negative integer in every window of size k/first-negative-integer-in-every-window-of-size-k.cpp
+++ Medium/First negative integer in every window of size k/first-negative-integer-in-every-window-of-size-k.cpp
@@ -29,35 +29,30 @@ int main() {
 // } Driver Code Ends
 
 
-vector<long long> printFirstNegativeInteger(long long int A[],
-                                             long long int N, long long int K) {
-                                                 
-      long long int i = 0, j = 0;
-    vector<long long> result;
-    deque<long long> negIndices; // Using deque to efficiently remove the first element
+vector<long long> printFirstNegativeInteger(long long int A[], long long int N, long long int K) {
+    int i = 0, j = 0;
+    queue<long long> ans;
+    vector<long long> fans;
 
     while (j < N) {
-        // If the current element is negative, store its index in the deque
-        if (A[j] < 0)
-            negIndices.push_back(j);
+        if (A[j] < 0) {
+            ans.push(A[j]);
+        }
 
-        // If the window size is less than K, expand the window
-        if (j - i + 1 < K)
+        if (j - i + 1 < K) {
             j++;
-        else { // If the window size is equal to K
-            // If there are no negative integers in the current window, push 0 to the result vector
-            if (negIndices.empty())
-                result.push_back(0);
-            else { // Otherwise, push the first negative integer to the result vector
-                result.push_back(A[negIndices.front()]);
-                // If the index of the first negative integer is the same as the start of the window, remove it from the deque
-                if (negIndices.front() == i)
-                    negIndices.pop_front();
+        } else if (j - i + 1 == K) {
+            if (ans.empty()) {
+                fans.push_back(0);
+            } else {
+                fans.push_back(ans.front());
+                if (A[i] == ans.front()) {
+                    ans.pop();
+                }
             }
-            i++; // Move the start of the window forward
-            j++; // Move the end of the window forward
+            i++;
+            j++;
         }
     }
-    return result;
-                                                 
- }
+    return fans;
+}
