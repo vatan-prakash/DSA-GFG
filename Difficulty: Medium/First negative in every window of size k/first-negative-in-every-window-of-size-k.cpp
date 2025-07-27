21280447868ec856bb8cf08bdb0b70f9fba23a91
vatan-prakash <90102762+vatan-prakash@users.noreply.github.com>
2025-07-27 11:14:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/First negative in every window of size k/first-negative-in-every-window-of-size-k.cpp
+++ Difficulty: Medium/First negative in every window of size k/first-negative-in-every-window-of-size-k.cpp
@@ -1,79 +1,36 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
     vector<int> firstNegInt(vector<int>& arr, int k) {
-        // write code here
-        vector<int> result;
-        deque<int> dq;
-        
-        int i=0;
-        int j=0;
-        
-        while(j<arr.size()){
-            
-            if(arr[j]<0){
-                dq.push_back(arr[j]);
-            }
-
-            
-           if(j-i+1==k){
-                if(dq.empty()){
-                        result.push_back(0);
-                }
+       vector<int> result;
+       deque<int> q;
+       int i=0;int j=0;
+       
+       
+       while(j<arr.size()){
+           if(arr[j]<0) q.push_back(arr[j]);
+         
+        //   if(j-i+1<k) j++;
+          
+           if(j-i+1==k) {
+             if(q.empty()){
+                 result.push_back(0);
+             }
+             else{
+                  result.push_back(q.front());
+             }
+             
+             if(!q.empty() && arr[i]==q.front()){
+                 q.pop_front();
                 
-                
-                else{
-                    result.push_back(dq.front());
-                }
-                
-                if(!dq.empty() && arr[i]==dq.front()){
-                    dq.pop_front();
-                }
-                
-                i++;
-            }
-            j++;
-        }
-        
-        return result;
+             }
+            i++;
+               
+           }
+           
+           j++;
+           
+           
+       }
+     return result;  
     }
-    
-    
 };
-
-
-//{ Driver Code Starts.
-int main() {
-    string ts;
-    getline(cin, ts);
-    int t = stoi(ts);
-    while (t--) {
-
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        string ks;
-        getline(cin, ks);
-        int k = stoi(ks);
-        Solution ob;
-        vector<int> ans = ob.firstNegInt(arr, k);
-        for (auto it : ans) {
-            cout << it << " ";
-        }
-        cout << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
